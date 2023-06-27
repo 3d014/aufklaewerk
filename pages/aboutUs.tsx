4b53fd50a91dfcdc7284183045584c36b027fba9
Damir Halilovic <dhalilov@dccsintra.net>
2023-06-27 16:37:44 +0200

    Remove temp labels and unneccesary libraries

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -3,11 +3,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { useTranslation } from "next-i18next"
 
 const AboutUsPage = () => {
-  return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <div style={{ height: "1000px", backgroundColor: "yellow", width: "100%" }}>Hello ne vii se jel</div>
-    </div>
-  )
+  return <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}></div>
 }
 
 export async function getStaticProps({ locale }) {
