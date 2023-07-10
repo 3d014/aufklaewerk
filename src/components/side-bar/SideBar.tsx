fc572b8f3d28bd74f32f1a6a954485ad2fab954b
edrapic <edrapic@capeannenterprises.com>
2023-07-10 19:23:35 +0200

    fixed side bar on small screens

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import MenuIcon from "@mui/icons-material/Menu"
 import { styles } from "../side-bar/SideBar.styles"
+import Link from "next/link"
+
+import { pages } from "@/src/routes"
 
 const SideBar = () => {
-  const [openDrawer, setOpenDrawer] = useState<boolean>(false)
+  const [openDrawer, setOpenDrawer] = useState(false)
+  
+
+  const handleLinkClick = () => {
+    setOpenDrawer(false)
+  }
+
+ 
 
   return (
     <>
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
         <List>
-          <ListItemButton>
-            <ListItemIcon>
-              <ListItemText>about us</ListItemText>
-            </ListItemIcon>
-          </ListItemButton>
+          {pages.map((page, index) => (
+            <Link href={page.path} passHref key={index}>
+              <ListItemButton  onClick={handleLinkClick}>
+                <ListItemIcon>
+                  <ListItemText>{page.text}</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+            </Link>
+          ))}
         </List>
       </Drawer>
       <IconButton
@@ -30,4 +44,5 @@ const SideBar = () => {
     </>
   )
 }
+
 export default SideBar
